c42661124a543b42a5f59b21f9f2ff73689439c8
Pavan Kumar <pavankumart46@gmail.com>
2020-04-24 16:22:45 +0530

    Solution for Video 1

--- app/src/main/java/youtube/com/vidhvan/databinding_viewmodel_livedata_starter/MainActivity.java
+++ app/src/main/java/youtube/com/vidhvan/databinding_viewmodel_livedata_starter/MainActivity.java
@@ -1,47 +1,39 @@
 package youtube.com.vidhvan.databinding_viewmodel_livedata_starter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import youtube.com.vidhvan.databinding_viewmodel_livedata_starter.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button mPlusBtn,mMinusBtn;
-    private TextView mScoreTv;
-
     private int score = 0;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
 
-        //Connecting the objects to Views using findViewById()
-        mPlusBtn = findViewById(R.id.plus_btn);
-        mMinusBtn = findViewById(R.id.minus_btn);
-        mScoreTv = findViewById(R.id.score);
-
-        // Handling Button Clicks
-        //Plus Button Click
-        mPlusBtn.setOnClickListener(new View.OnClickListener() {
+        binding.plusBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Incrementing the score by 1 & Updating it on the TextView
                 score++;
-                mScoreTv.setText(String.valueOf(score));
+                binding.score.setText(String.valueOf(score));
             }
         });
 
-        //Minus Button Click
-        mMinusBtn.setOnClickListener(new View.OnClickListener() {
+        binding.minusBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Decrementing the score by 1 & Updating it on the TextView
                 score--;
-                mScoreTv.setText(String.valueOf(score));
+                binding.score.setText(String.valueOf(score));
             }
         });
     }
